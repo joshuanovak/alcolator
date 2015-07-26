f7a9dd6dab6e912da4a3cf7128bfed57acbb38f1
Joshua Novak <josh19novak@gmail.com>
2015-07-25 22:57:04 -0500

    Assignment 19

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Joshua on 7/24/15.
+//  Copyright (c) 2015 Joshua Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
