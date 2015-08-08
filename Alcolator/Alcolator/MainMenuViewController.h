262e3237346a7ec31ebe1b3576aa3d855483e4df
Joshua Novak <josh19novak@gmail.com>
2015-08-08 11:57:54 -0500

    Assignment 21

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Joshua on 8/5/15.
+//  Copyright (c) 2015 Joshua Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
